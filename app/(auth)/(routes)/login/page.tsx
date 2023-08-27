8cc9ee813e1fb64856ecebe565a2f4ac97127b55
Manuj Sankrit <msankrit8055@gmail.com>
2023-08-27 21:58:22 +0530

    Initial commit

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+  return <div>LoginPage (unprotected)</div>;
+}
+
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Hello AI Elites (protected)</div>;
+}
