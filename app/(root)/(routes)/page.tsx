4a16dc0fc8ce9db3f5774004ebedbaf5362c8585
Manuj Sankrit <msankrit8055@gmail.com>
2023-08-28 21:40:44 +0530

    Added category filter
    Incorporated Planetscale headless service for hosting mysql database, used prisma as ORM

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,14 @@
-'use client';
-
+import Categories from '@/components/categories';
 import SearchInput from '@/components/search-input';
+import prismaDB from '@/lib/prismadb';
 
-export default function Home() {
+// eslint-disable-next-line @next/next/no-async-client-component
+export default async function Home() {
+  const categories = await prismaDB.category.findMany();
   return (
     <div className='h-screen p-4 space-y-2'>
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 }
