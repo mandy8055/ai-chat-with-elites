55685dc0be899d9e965246ef6e343aba49686eaf
Manuj Sankrit <msankrit8055@gmail.com>
2023-08-31 08:00:18 +0530

    Wired up chatUI
    Enhanced User behaviour via fake loading experience

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCompletion } from 'ai/react';
 import ChatHeader from '@/components/chat-header';
 import { Companion, Message } from '@prisma/client';
+import ChatForm from '@/components/chat-form';
+import ChatMessages from '@/components/chat-messages';
+import { ChatMessageProps } from '@/components/chat-message';
 
 interface ChatClientProps {
   companion: Companion & {
@@ -13,9 +19,48 @@ interface ChatClientProps {
 }
 
 const ChatClient = ({ companion }: ChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatMessageProps[]>(
+    companion.messages,
+  );
+
+  const { input, isLoading, handleInputChange, handleSubmit, setInput } =
+    useCompletion({
+      api: `/api/chat/${companion.id}`,
+      onFinish(prompt, completion) {
+        const systemMessage: ChatMessageProps = {
+          role: 'system',
+          content: completion,
+        };
+        setMessages((messages) => [...messages, systemMessage]);
+        setInput('');
+        router.refresh();
+      },
+    });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const userMessage: ChatMessageProps = {
+      role: 'user',
+      content: input,
+    };
+    setMessages((messages) => [...messages, userMessage]);
+    handleSubmit(e);
+  };
+
   return (
     <div className='flex flex-col h-screen p-4 space-y-2'>
       <ChatHeader companion={companion} />
+      <ChatMessages
+        messages={messages}
+        isLoading={isLoading}
+        companion={companion}
+      />
+      <ChatForm
+        isLoading={isLoading}
+        input={input}
+        handleInputChange={handleInputChange}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Avatar, AvatarImage } from './ui/avatar';
 
 interface BotAvatarProps {
@@ -6,7 +5,6 @@ interface BotAvatarProps {
 }
 
 const BotAvatar = ({ src }: BotAvatarProps) => {
-  console.log({ src });
   return (
     <Avatar className='h-12 w-12'>
       <AvatarImage src={src} />
