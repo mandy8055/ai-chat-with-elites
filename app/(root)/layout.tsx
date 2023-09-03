7f1fee2be2c444619a9a27a2796a248526e46c0b
Manuj Sankrit <msankrit8055@gmail.com>
2023-09-03 09:10:42 +0530

    Tried fixing the import bug for build

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/navbar';
+import { Navbar } from '@/components/navbar';
 import Sidebar from '@/components/sidebar';
 import { checkSubscription } from '@/lib/subscription';
 
