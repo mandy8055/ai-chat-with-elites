8c06144eae2ae85f0fc364685e41febddf19b1c8
Manuj Sankrit <msankrit8055@gmail.com>
2023-09-03 09:17:57 +0530

    Versioning untracked files

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,7 +19,7 @@ interface NavbarProps {
   isPro: boolean;
 }
 
-export const Navbar = ({ isPro }: NavbarProps) => {
+const Navbar = ({ isPro }: NavbarProps) => {
   const proModal = useProModalStore();
   return (
     <div className='fixed w-screen z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary'>
@@ -49,3 +49,5 @@ export const Navbar = ({ isPro }: NavbarProps) => {
     </div>
   );
 };
+
+export default Navbar;
